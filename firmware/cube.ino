44ff89ac689ad51532b1774ac560e023b77ca693
Evan Yand <evanyand@gmail.com>
2016-10-28 20:51:00 -0700

    started template for firmware

---
+++ firmware/cube.ino
@@ -0,0 +1,47 @@
+#include <tlc_shifts.h>
+#include <tlc_servos.h>
+#include <tlc_animations.h>
+#include <tlc_fades.h>
+#include <tlc_progmem_utils.h>
+#include <tlc_config.h>
+#include <Tlc5940.h>
+
+
+//Pin Definitions
+#define TLC_SIN    1
+#define TLC_SCLK   2
+#define TLC_GSCLK  2
+#define TLC_XLAT   2
+#define TLC_BLANK  2
+#define TLC_VPRG   2
+
+#define MSG_OUT    3
+#define MSG_STRBE  2
+#define MSG_RST    2
+
+#define LYR1       2
+#define LYR2       2
+#define LYR3       2
+
+
+//Code
+void setup(){
+
+
+
+
+
+
+}
+
+
+void loop(){
+
+
+
+
+
+}
+
+//Functions
+
